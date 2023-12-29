77109ea8db6248840a0c5e80b5b7093dc9053fc3
goforbroke <go.for.broke1006@gmail.com>
2023-12-30 01:02:03 +0200

    Initial commit

--- main.cpp
+++ main.cpp
@@ -10,19 +10,27 @@
 #include "Debug.h"
 
 int main() {
-    LuaCpp::LuaContext luaCtx;
-    luaCtx.CompileStringAndRun("print('Hello World!!!')");
+    LuaCpp::LuaContext ctx;
+    ctx.CompileStringAndRun("print('Hello World!!!')");
 
-    std::shared_ptr<LuaCpp::Registry::LuaLibrary> lib = std::make_shared<LuaCpp::Registry::LuaLibrary>("Debug");
-    lib->AddCFunction("Log", DebugLog);
-    lib->AddCFunction("LogWarning", DebugLogWarning);
-    lib->AddCFunction("LogError", DebugLogError);
-    luaCtx.AddLibrary(lib);
+    /*try {
+        std::shared_ptr<LuaCpp::Registry::LuaLibrary> lib = std::make_shared<LuaCpp::Registry::LuaLibrary>("Debug");
+        lib->AddCFunction("Log", DebugLog);
+        lib->AddCFunction("LogWarning", DebugLogWarning);
+        lib->AddCFunction("LogError", DebugLogError);
+        ctx.AddLibrary(lib);
+
+        ctx.CompileStringAndRun("Debug.Log('Hello from function!!!')");
+        ctx.CompileStringAndRun("Debug.LogWarning('Hello from function!!!')");
+        ctx.CompileStringAndRun("Debug.LogError('Hello from function!!!')");
+    } catch (const std::runtime_error &exc) {
+        std::cerr << exc.what() << std::endl;
+    }*/
 
     try {
-        luaCtx.CompileStringAndRun("Debug.Log('Hello from function!!!')");
-        luaCtx.CompileStringAndRun("Debug.LogWarning('Hello from function!!!')");
-        luaCtx.CompileStringAndRun("Debug.LogError('Hello from function!!!')");
+        ctx.CompileFolder("./Scripts/", "Scripts", false);
+        ctx.CompileStringAndRun("require 'Scripts/Debug' \n"
+                                "Debug.Log('Gotcha!')");
     } catch (const std::runtime_error &exc) {
         std::cerr << exc.what() << std::endl;
     }
